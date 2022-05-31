a1fddd22212b781aaf1ecd9ae80e6363b7b9e959
CCrocilla <cla.crocilla@gmail.com>
2022-05-31 23:22:07 +0000

    Deployment to Heroku

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
 ''' Imports '''
 import os
 import json
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 @app.route("/")
@@ -34,9 +36,14 @@ def about_member(member_name):
     return render_template("member.html", member=members)
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
     ''' Contact Us Page '''
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message!".format(
+            request.form.get("name")))
+        # print(request.form.get("name"))
+        # print(request.form["email"])
     return render_template("contact.html", page_title="Contact")
 
 
